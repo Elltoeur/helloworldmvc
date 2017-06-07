78ad9dec50e667986fa3a02fc4380d6338d5a930
Elltoeur <flavien.msn@hotmail.fr>
2017-06-07 09:36:12 +0200

    a

---
+++ helloworldmvc/helloworldmvc.model/src/main/java/helloworldmvc/model/DAOHelloWorld.java
@@ -0,0 +1,10 @@
+package helloworldmvc.model;
+
+public class DAOHelloWorld {
+	private String FileName = "hello.txt";
+	private instance DAOHelloworld = null;
+	private String helloWorldMessage =null;
+	
+	private DAOHelloWorld (){};
+	
+}
